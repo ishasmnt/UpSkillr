0ac5bbe755a299b619a156e33e539441f9ed6f6a
Isha_30 <ishasmnt30@gmail.com>
2026-01-09 22:21:39 +0530

    Delete controllers directory

--- controllers/authController.js
+++
@@ -1,9 +0,0 @@
-const register = (req, res) => {
-  res.json({ message: "User registered" });
-};
-
-const login = (req, res) => {
-  res.json({ message: "User logged in" });
-};
-
-module.exports = { register, login };
